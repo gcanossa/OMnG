358e51f47d2614f3f5c6288439cc7baea49c8224
goldenass <gianmaria.canossa@gmail.com>
2018-03-09 09:58:02 +0100

    labelling

---
+++ src/OMnG/TypeExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace OMnG
+{
+    public static class TypeExtensions
+    {
+        public static string EscapeName(this string name)
+        {
+            return name?.Replace(".","_").Replace("+","__");
+        }
+        public static string UnescapeName(this string name)
+        {
+            return name?.Replace("__", "+").Replace("_", ".");
+        }
+
+        public static string GetLablel(this object obj)
+        {
+            return GetLabel(obj?.GetType());
+        }
+        public static string GetLabel<T>()
+        {
+            return GetLabel(typeof(T));
+        }
+        public static string GetLabel(this Type type)
+        {
+            type = type ?? throw new ArgumentNullException(nameof(type));
+
+            return type.FullName.EscapeName();
+        }
+
+        public static IEnumerable<string> GetLabels(this object obj)
+        {
+            return GetLabels(obj?.GetType());
+        }
+        public static IEnumerable<string> GetLabels<T>()
+        {
+            return GetLabels(typeof(T));
+        }
+        public static IEnumerable<string> GetLabels(this Type type)
+        {
+            type = type ?? throw new ArgumentNullException(nameof(type));
+
+            HashSet<string> result = new HashSet<string>();
+            result.Add(GetLabel(type));
+
+            foreach (Type item in type.GetInterfaces())
+            {
+                result.Add(GetLabel(item));
+            }
+
+            while (type != typeof(object) && !type.IsInterface)
+            {
+                type = type.BaseType;
+                if (type != typeof(object))
+                    result.Add(GetLabel(type));
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<Type> GetTypesFromLabels(this IEnumerable<string> labels)
+        {
+            if (labels == null)
+                throw new ArgumentNullException(nameof(labels));
+
+            IEnumerable<Type> types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(p => p.GetTypes());
+
+            return labels.Select(p => types.FirstOrDefault(x => x.FullName.EscapeName() == p));
+        }
+        
+        public static object GetInstanceOfMostSpecific(this IEnumerable<Type> types)
+        {
+            types = types ?? throw new ArgumentNullException(nameof(types));
+
+            Type type = null;
+            foreach (Type t in types.Where(p=>!p.IsInterface && !p.IsAbstract && p.GetConstructor(new Type[0])!=null))
+            {
+                if (type == null || type.IsAssignableFrom(t))
+                    type = t;
+            }
+
+            if (type == null)
+                throw new InvalidOperationException("None of the provided types can be used for instantiation");
+
+            return Activator.CreateInstance(type);
+        }
+
+        public static bool CheckObjectInclusion(this object obj, object included)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+
+            if (included == null)
+                return false;
+
+            Type type = obj.GetType();
+            foreach (PropertyInfo pinfo in included.GetType().GetProperties().Where(p => p.CanRead))
+            {
+                PropertyInfo tmp = type.GetProperty(pinfo.Name);
+                if (
+                    tmp == null ||
+                    pinfo.PropertyType != tmp.PropertyType ||
+                    (pinfo.GetValue(included) == null && tmp.GetValue(obj) != null) ||
+                    (pinfo.GetValue(included) != null && tmp.GetValue(obj) == null) ||
+                    (pinfo.GetValue(included) != null && tmp.GetValue(obj) != null && !pinfo.GetValue(included).Equals(tmp.GetValue(obj))))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ test/UnitTest/TypeExtensionsTests.cs
@@ -0,0 +1,110 @@
+﻿using OMnG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace UnitTest
+{
+    public class TypeExtensionsTests
+    {
+        #region nested type
+
+        public interface IInterfaceA
+        {
+            string PropA { get; set; }
+            string ShadowPropA { get; }
+        }
+        public interface IInterfaceB { }
+        public interface IInterfaceC : IInterfaceA, IInterfaceB { }
+
+        public abstract class AbstractClass
+        {
+            public int PropAbstract { get; set; }
+        }
+
+        public class ClassA
+        {
+            public string PropA { get; set; }
+        }
+        public class ClassNotA
+        {
+            public int PropA { get; set; }
+        }
+
+        public class ClassB : AbstractClass, IInterfaceA
+        {
+            public string PropA { get; set; }
+            public string ShadowPropA { get; }
+        }
+
+        public class ClassC : AbstractClass, IInterfaceC
+        {
+            public string PropA { get; set; }
+            public string ShadowPropA { get; }
+            public ClassA PropC { get; set; }
+        }
+
+        #endregion
+
+        [Trait("Category", nameof(TypeExtensionsTests))]
+        [Fact(DisplayName = nameof(GetLablel))]
+        public void GetLablel()
+        {
+            Assert.Equal("UnitTest_TypeExtensionsTests__ClassC", TypeExtensions.GetLabel<ClassC>());
+        }
+
+        [Trait("Category", nameof(TypeExtensionsTests))]
+        [Fact(DisplayName = nameof(GetLablels))]
+        public void GetLablels()
+        {
+            Assert.Equal(new string[]
+            {
+                "UnitTest.TypeExtensionsTests+ClassC".EscapeName(),
+                "UnitTest.TypeExtensionsTests+IInterfaceC".EscapeName(),
+                "UnitTest.TypeExtensionsTests+IInterfaceA".EscapeName(),
+                "UnitTest.TypeExtensionsTests+IInterfaceB".EscapeName(),
+                "UnitTest.TypeExtensionsTests+AbstractClass".EscapeName()
+            }, TypeExtensions.GetLabels<ClassC>());
+        }
+
+        [Trait("Category", nameof(TypeExtensionsTests))]
+        [Fact(DisplayName = nameof(GetTypesFromLabels))]
+        public void GetTypesFromLabels()
+        {
+            Assert.Equal(new Type[]
+            {
+                typeof(ClassC),
+                typeof(IInterfaceC),
+                typeof(IInterfaceA),
+                typeof(IInterfaceB),
+                typeof(AbstractClass)
+            }, TypeExtensions.GetLabels<ClassC>().GetTypesFromLabels());
+        }
+        
+        [Trait("Category", nameof(TypeExtensionsTests))]
+        [Fact(DisplayName = nameof(GetInstanceOfMostSpecific))]
+        public void GetInstanceOfMostSpecific()
+        {
+            var types = TypeExtensions.GetLabels<ClassC>().GetTypesFromLabels();
+
+            Assert.True(types.GetInstanceOfMostSpecific() is ClassC);
+
+            Assert.Throws<InvalidOperationException>(() => types.Where(p => p != typeof(ClassC)).GetInstanceOfMostSpecific());
+        }
+        
+        [Trait("Category", nameof(TypeExtensionsTests))]
+        [Fact(DisplayName = nameof(CheckObjectInclusion))]
+        public void CheckObjectInclusion()
+        {
+            ClassC c = new ClassC();
+            ClassA a = new ClassA();
+            ClassNotA na = new ClassNotA();
+            IInterfaceA ia = null;
+
+            Assert.True(c.CheckObjectInclusion(a));
+            Assert.False(c.CheckObjectInclusion(ia));
+            Assert.False(c.CheckObjectInclusion(na));
+        }
+    }
+}
